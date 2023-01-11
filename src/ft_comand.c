88629737248652485c8d92e58b9837297160343f
Eric Alonso Martinez <eralonso@cbr14s1.42barcelona.com>
2023-01-11 19:30:35 +0100

    hola quiero ser joe

--- src/ft_comand.c
+++ src/ft_comand.c
@@ -6,7 +6,7 @@
 /*   By: eralonso <eralonso@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 19:22:33 by eralonso          #+#    #+#             */
-/*   Updated: 2023/01/11 13:26:47 by eralonso         ###   ########.fr       */
+/*   Updated: 2023/01/11 18:19:07 by eralonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,7 +72,7 @@ char	**ft_clean_cmd(char *cmd_tot)
 	char	**cmd_clean;
 
 	i = 0;
-	j = 0;
+	j = -1;
 	cmd_clean = (char **)ft_calloc(sizeof(char *), ft_arg_num(cmd_tot) + 1);
 	if (!cmd_clean)
 		return (0);
@@ -88,36 +88,37 @@ char	**ft_clean_cmd(char *cmd_tot)
 			i++;
 		if (cmd_tot[i] && !(ft_strchr("\'\" \0", cmd_tot[i]) && (!i
 					|| (i && cmd_tot[i - 1] != '\\'))))
-			j++;
+		{
+			cmd_clean[++j] = ft_fill_arg(&cmd_tot[i], del);
+			if (!cmd_clean[j])
+				return (ft_free(cmd_clean, 1));
+		}
 		while (cmd_tot[i] && cmd_tot[i] != del)
-			cmd_clean[j][k + i] = cmd_tot[i++];
+			i++;
 		i += (cmd_tot[i] != '\0') * (ft_strchr("\'\" \0", cmd_tot[i]) && (!i
 					|| (i && cmd_tot[i - 1] != '\\')));
 	}
 	return (cmd_clean);
 }
 
-int	ft_clean_size(char *cmd_tot)
+int	ft_arg_len(char *cmd_tot, char del)
 {
-	int	size;
-	int	i;
+	int		size;
+	int		i;
+	char	*arg;
 
 	size = 0;
-	i = 0;
-	while (cmd_tot[i])
-	{
-		while (cmd_tot[i] && cmd_tot[i] != '\\')
-		{
-			if (!ft_strchr("\"\'\0", cmd_tot[i]))
-				size++;
-			i++;
-		}
-		if (cmd_tot[i] && cmd_tot[i + 1] && cmd_tot[i] == '\\'
-			&& ft_strchr("\"\'\\\0", cmd_tot[i + 1]))
-				size++;
-		i += (cmd_tot[i] != '\0');
-	}
-	return (size);
+	i = -1;
+	if (!cmd_tot)
+		return (NULL);
+	while (cmd_tot[size] && cmd_tot[size] != del)
+		size++;
+	arg = (char *)ft_calloc(sizeof(char), size + 1);
+	if (!arg)
+		return (NULL);
+	while (cmd_tot[++i] && cmd_tot[i] != del)
+		arg[i] = cmd_tot[i];
+	return (arg);
 }
 
 int	ft_arg_num(char *args)
@@ -128,7 +129,6 @@ int	ft_arg_num(char *args)
 
 	i = 0;
 	size = 0;
-	while (args[i])
 	{
 		del = ' ';
 		while (args[i] && args[i] == ' ')
